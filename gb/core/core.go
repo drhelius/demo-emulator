e6f3a8cd79f85d44a71f9d86e0e51ee82b9e5423
Ignacio Sanchez Gines <nacho@geardome.com>
2016-10-10 23:36:50 +0200

    minor refactor

--- gb/core/core.go
+++ gb/core/core.go
@@ -24,18 +24,16 @@ var (
 // RunToVBlank runs a single frame of the emulator
 // The emulator must run at 60fps
 func RunToVBlank(colorFrameBuffer []uint8) {
-	if ready {
-		for vblank := false; !vblank; {
-			var clockCycles = cpu.Tick()
-			vblank = video.Tick(clockCycles)
-			input.Tick(clockCycles)
-		}
+	for vblank := false; !vblank; {
+		var clockCycles = cpu.Tick()
+		vblank = video.Tick(clockCycles)
+		input.Tick(clockCycles)
+	}
 
-		for i, pixelCount := 0, util.GbWidth*util.GbHeight; i < pixelCount; i++ {
-			colorFrameBuffer[i*4] = pallete[video.GbFrameBuffer[i]*3]         // red
-			colorFrameBuffer[(i*4)+1] = pallete[(video.GbFrameBuffer[i]*3)+1] // green
-			colorFrameBuffer[(i*4)+2] = pallete[(video.GbFrameBuffer[i]*3)+2] // blue
-		}
+	for i, pixelCount := 0, util.GbWidth*util.GbHeight; i < pixelCount; i++ {
+		colorFrameBuffer[i*4] = pallete[video.GbFrameBuffer[i]*3]         // red
+		colorFrameBuffer[(i*4)+1] = pallete[(video.GbFrameBuffer[i]*3)+1] // green
+		colorFrameBuffer[(i*4)+2] = pallete[(video.GbFrameBuffer[i]*3)+2] // blue
 	}
 }
 
@@ -88,8 +86,6 @@ func LoadROM(filePath string) {
 	m.Setup(data)
 	cpu.SetMapper(m)
 	video.SetMapper(m)
-
-	ready = true
 }
 
 // ButtonPressed tells the emulator that a button has been pressed

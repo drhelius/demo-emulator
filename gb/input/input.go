779dbd139c54de4b09c10ed910dbb7ece056e899
Ignacio Sanchez Gines <isanchez.gines@atsistemas.com>
2016-10-11 10:12:55 +0200

    input implemented

--- gb/input/input.go
+++ gb/input/input.go
@@ -1,31 +1,70 @@
 package input
 
 import (
-	"fmt"
-
+	"github.com/drhelius/demo-emulator/gb/cpu"
 	"github.com/drhelius/demo-emulator/gb/util"
 )
 
+var (
+	joypadState uint8
+	p1          uint8
+	inputCycles uint32
+)
+
+func init() {
+	joypadState = 0xFF
+	p1 = 0xFF
+}
+
 // Tick runs the input eumulation n cycles
 func Tick(cycles uint32) {
+	inputCycles += cycles
 
+	// Joypad Poll Speed (64 Hz)
+	if inputCycles >= 65536 {
+		inputCycles -= 65536
+		update()
+	}
 }
 
 // Read returns the 8 bit value at the 16 bit address of the memory
 func Read() uint8 {
-	return 0xFF
+	return p1
 }
 
 // Write stores the 8 bit value at the 16 bit address of the memory
 func Write(value uint8) {
+	p1 = (p1 & 0xCF) | (value & 0x30)
+	update()
 }
 
 // ButtonPressed tells the input system that a button has been pressed
 func ButtonPressed(button util.GameboyButton) {
-	fmt.Printf("button pressed %d\n", button)
+	joypadState = util.UnsetBit(joypadState, uint8(button))
 }
 
 // ButtonReleased tells the input system that a button has been released
 func ButtonReleased(button util.GameboyButton) {
-	fmt.Printf("button released %d\n", button)
+	joypadState = util.SetBit(joypadState, uint8(button))
+}
+
+func update() {
+	current := p1 & 0xF0
+
+	switch current & 0x30 {
+	case 0x10:
+		topJoypad := (joypadState >> 4) & 0x0F
+		current |= topJoypad
+	case 0x20:
+		bottomJoypad := joypadState & 0x0F
+		current |= bottomJoypad
+	case 0x30:
+		current |= 0x0F
+	}
+
+	if (p1 & ^current & 0x0F) != 0 {
+		cpu.RequestInterrupt(cpu.InterruptJoypad)
+	}
+
+	p1 = current
 }

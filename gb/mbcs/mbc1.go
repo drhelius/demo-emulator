acbbf490d45f412e3e7f1fcf092f30f7314710ae
Ignacio Sanchez Gines <nacho@geardome.com>
2016-10-14 00:51:26 +0200

    fixed bug calculating mbc1 rom banks

--- gb/mbcs/mbc1.go
+++ gb/mbcs/mbc1.go
@@ -8,11 +8,11 @@ type MBC1 struct {
 	rom             []uint8
 	ram             []uint8
 	mode            uint8
-	romBank         uint16
+	romBank         uint32
 	ramBank         uint16
 	ramEnabled      bool
-	romBankHighBits uint8
-	higherROMBank   uint16
+	romBankHighBits uint32
+	higherROMBank   uint32
 	higherRAMBank   uint16
 	ramSize         uint8
 }
@@ -47,14 +47,12 @@ func (m *MBC1) Setup(r []uint8) {
 		fallthrough
 	case 0x02:
 		m.higherRAMBank = 0x00
-	case 0x04:
-		m.higherRAMBank = 0x0F
 	default:
 		m.higherRAMBank = 0x03
 		break
 	}
 	fmt.Printf("the higher RAM bank is %X\n", m.higherRAMBank)
-	m.higherROMBank = uint16(max(pow2Ceil(len(m.rom)/0x4000), 2) - 1)
+	m.higherROMBank = uint32(max(pow2Ceil(len(m.rom)/0x4000), 2) - 1)
 	fmt.Printf("the higher ROM bank is %X\n", m.higherROMBank)
 }
 
@@ -66,7 +64,7 @@ func (m *MBC1) Read(addr uint16) uint8 {
 		return m.rom[addr]
 	case (addr >= 0x4000) && (addr < 0x8000):
 		// ROM bank X
-		return m.rom[(addr-0x4000)+(m.romBank*0x4000)]
+		return m.rom[(uint32(addr)-0x4000)+(m.romBank*0x4000)]
 	case (addr >= 0xA000) && (addr < 0xC000):
 		// RAM bank
 		if m.ramEnabled {
@@ -88,17 +86,14 @@ func (m *MBC1) Read(addr uint16) uint8 {
 func (m *MBC1) Write(addr uint16, value uint8) {
 	switch {
 	case (addr >= 0x0000) && (addr < 0x2000):
-		//fmt.Printf("** write %X %X\n", addr, value)
 		if m.ramSize > 0 {
 			m.ramEnabled = ((value & 0x0F) == 0x0A)
-			//fmt.Println("** ramEnabled ", m.ramEnabled)
 		}
 	case (addr >= 0x2000) && (addr < 0x4000):
-		//fmt.Printf("** write %X %X\n", addr, value)
 		if m.mode == 0 {
-			m.romBank = uint16((value & 0x1F) | (m.romBankHighBits << 5))
+			m.romBank = uint32(value&0x1F) | (m.romBankHighBits << 5)
 		} else {
-			m.romBank = uint16(value & 0x1F)
+			m.romBank = uint32(value & 0x1F)
 		}
 
 		if m.romBank == 0x00 || m.romBank == 0x20 || m.romBank == 0x40 || m.romBank == 0x60 {
@@ -106,34 +101,27 @@ func (m *MBC1) Write(addr uint16, value uint8) {
 		}
 
 		m.romBank &= m.higherROMBank
-		//fmt.Println("** romBank ", m.romBank)
 	case (addr >= 0x4000) && (addr < 0x6000):
-		//fmt.Printf("** write %X %X\n", addr, value)
 		if m.mode == 1 {
 			m.ramBank = uint16(value & 0x03)
 			m.ramBank &= m.higherRAMBank
-			//fmt.Println("** ramBank ", m.ramBank)
 		} else {
-			m.romBankHighBits = value & 0x03
-			m.romBank = (m.romBank & 0x1F) | (uint16(m.romBankHighBits) << 5)
+			m.romBankHighBits = uint32(value & 0x03)
+			m.romBank = (m.romBank & 0x1F) | (m.romBankHighBits << 5)
 
 			if m.romBank == 0x00 || m.romBank == 0x20 || m.romBank == 0x40 || m.romBank == 0x60 {
 				m.romBank++
 			}
 
 			m.romBank &= m.higherROMBank
-			//fmt.Println("** romBank ", m.romBank)
 		}
 	case (addr >= 0x6000) && (addr < 0x8000):
-		//fmt.Printf("** write %X %X\n", addr, value)
 		if (m.ramSize != 3) && ((value & 0x01) != 0) {
 			fmt.Printf("** Attempting to change MBC1 to mode 1 with incorrect RAM banks %X %X\n", addr, value)
 		} else {
 			m.mode = value & 0x01
 		}
-		//fmt.Println("** mode ", m.mode)
 	case (addr >= 0xA000) && (addr < 0xC000):
-		//fmt.Printf("** write %X %X\n", addr, value)
 		if m.ramEnabled {
 			if m.mode == 0 {
 				m.ram[addr-0xA000] = value
